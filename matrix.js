cd2a461af77e9ff6048d93c2ab59f405ad1339db
RigaGames <rigagamestd@gmail.com>
2022-01-05 23:57:11 +0300

    clear cache

--- matrix.js
+++ matrix.js
@@ -14,6 +14,7 @@ window.addEventListener('resize', function(event) {
     ch = window.innerHeight;
     canvas.width = cw
     canvas.height = ch;
+    maxColumns = cw / fontSize;
     console.log(cw, ch)
 }, true);
 
