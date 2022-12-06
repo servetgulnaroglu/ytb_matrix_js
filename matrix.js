053f20465f41e070a7c9e4aebf625ffca58a7ffd
Dany Cervantes <danycervantesq@gmail.com>
2022-12-05 19:14:42 -0500

    Update matrix.js

--- matrix.js
+++ matrix.js
@@ -1,33 +1,48 @@
 const canvas = document.getElementById("canvas");
 const canvas2D = canvas.getContext("2d");
-const charArray = [
-    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
-    "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", 
-    "1", "2", "3", "4", "5", "6", "7", "8", 
-    "A", "B", "Г", "Δ", "Є", "Z", "H", "Ѳ", "I", "K", "Λ", "M", //Greek Alphabet
-    "N", "Ξ", "O", "П", "P", "Σ", "T", "Y", "Φ", "X", "	Ψ", "Ω",
-]
 
-let screenHeight = window.innerHeight;
-let screenWidth = window.innerWidth;
-
-let maxChar = 300;
-let fallArray = [];
-let fontSize = 13;
-let maxColumns = screenWidth / fontSize;
+let fallArr = [];
+let fontSize = 12;
 let frames = 0;
+let limit = 275;
 
-canvas.width = screenWidth;
-canvas.height = screenHeight;
+let maxColumns;
+let screenHeight;
+let screenWidth; 
 
-class Falling {
+let charArr = [
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", //Roman Alphabet
+    "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", 
+    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+    "A", "B", "Г", "Δ", "Є", "Z", "H", "Ѳ", "I", "K", "Λ", "M", //Greek Alphabet
+    "N", "Ξ", "O", "П", "P", "Σ", "T", "Y", "Φ", "X", "	Ψ", "Ω"
+];
+
+
+//sets height and width of display
+function displayOn() {
+    screenHeight = window.innerHeight;
+    screenWidth = window.innerWidth;
+    canvas.height = screenHeight;
+    canvas.width = screenWidth;
+    maxColumns = screenWidth / fontSize;
+}
+
+//Returns a random number within a chosen range
+function randomRange(min,max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+//Math.floor() rounds down to the nearest whole number  e.i. 10 = 0 - 9  
+//Math.random() returns a random decimal between 0 - 0.99
+}
+
+class Fall {
     constructor(x, y) {
         this.x = x;
         this.y = y;
     }
 
     draw(canvas2D) {
-        this.value = charArray[ Math.floor(Math.random() * charArray.length) ].toUpperCase();
+        this.value = charArr[randomRange(0, charArr.length - 1)];
 
         this.speed = ((Math.random() * fontSize * 3) / 4) + ((fontSize * 3) / 4);
 
@@ -45,19 +60,19 @@ class Falling {
 }
 
 let update = () => {
-    if(fallArray.length < maxChar) {
+    if(fallArr.length < limit) {
 
-        let character = new Falling(Math.floor(Math.random() * maxColumns) * fontSize, ((Math.random() * screenHeight) / 2) - 50 );
+        let character = new Fall(Math.floor(Math.random() * maxColumns) * fontSize, ((Math.random() * screenHeight) / 2) - 50 );
 
-        fallArray.push(character);
+        fallArr.push(character);
     }
 
     canvas2D.fillStyle = "rgba(0,0,0,0.05)";
     canvas2D.fillRect(0, 0, screenWidth, screenHeight);
 
-    for(let i = 0; i < fallArray.length && frames % 2 == 0; i++) {
+    for(let i = 0; i < fallArr.length && frames % 2 == 0; i++) {
 
-        fallArray[i].draw(canvas2D);
+        fallArr[i].draw(canvas2D);
     }
 
     requestAnimationFrame(update);
@@ -65,18 +80,17 @@ let update = () => {
 };
 
 
-//reassigns value to screenWidth if screen size changes
+//reassigns values if screen size changes
 window.addEventListener("resize", function() {
-    screenWidth = window.innerWidth;
-    screenHeight = window.innerHeight;
-    canvas.width = screenWidth
-    canvas.height = screenHeight;
-    maxColumns = screenWidth / fontSize;
+
+   displayOn();
+
 });
 
 
 window.onload = function() {
 
-    update();
+    displayOn();
 
+    update();
 };
